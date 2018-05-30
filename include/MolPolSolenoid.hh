7340c498311df7e19b530894d164796c84984f00
dericking <dekin100@syr.edu>
2018-05-30 15:58:42 -0400

    Added MolPolSolenoid::UpdateSolenoid() ... and tidied up code.

--- include/MolPolSolenoid.hh
+++ include/MolPolSolenoid.hh
@@ -8,26 +8,21 @@
 class MolPolSolenoid : public G4MagneticField
 {
   public: // with description
-
-  MolPolSolenoid(G4double          Bz, G4double fz,
-		 G4ThreeVector     pOrigin);
-    
+    MolPolSolenoid(G4double Bz, G4double fz, G4ThreeVector pOrigin);
     ~MolPolSolenoid();
-  
-    void GetFieldValue(const G4double yTrack[],
-                             G4double B[]     ) const;
+    
+    void GetFieldValue(const G4double yTrack[], G4double B[] ) const;
+    void UpdateSolenoid(G4double Bz, G4double fz, G4ThreeVector pOrigin);
+
   private:
+    G4double          fBfield;
+    G4double          fFringeZ;
+    G4double          fFieldLength;
+    G4double          fFieldRadius;
+    G4ThreeVector     fOrigin;
 
-  G4double          fBfield;
-  G4double          fFringeZ;
-  G4double          fFieldLength;
-  G4double          fFieldRadius;
-  G4ThreeVector     fOrigin;
-
-  G4bool IsOutside(G4ThreeVector& local) const;
-  G4bool IsWithin(G4ThreeVector& local) const;
-
+    G4bool IsOutside(G4ThreeVector& local) const;
+    G4bool IsWithin(G4ThreeVector& local) const;
 
 };
 #endif
-
