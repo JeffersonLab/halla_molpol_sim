ebe3cfa18a6e442d8c66ff89f0041429155b0ab0
dericking <dekin100@syr.edu>
2018-07-23 11:33:46 -0400

    elim uniformField booleans -- unnecessary

--- include/MolPolEMField.hh
+++ include/MolPolEMField.hh
@@ -18,7 +18,7 @@ class MolPolEMField : public G4ElectroMagneticField
 public:
 
 	MolPolEMField() ;
-	MolPolEMField( std::vector<G4String> files , std::vector<G4double> scales , std::vector<G4double> beamlineOffsets);
+	MolPolEMField( std::vector<G4String> files , std::vector<G4double> scales , std::vector<G4double> beamlineOffsets );
 	~MolPolEMField() ;
 
 	inline void GetFieldValue(const G4double Point[4], G4double *Bfield ) const;
@@ -37,25 +37,21 @@ public:
 	inline void SetErInner(G4double val) { ErInner = val; }
 	inline G4double GetErInner() const { return ErInner; }
 
-	inline void SetEField3V(G4ThreeVector v) { EField3V = v; bUseUniformEField=true;}
+	inline void SetEField3V(G4ThreeVector v) { EField3V = v; }
 	inline G4ThreeVector GetEField3V() const { return EField3V; }
 
-	inline void SetBField3V(G4ThreeVector v) { BField3V = v; bUseUniformBField=true;}
+	inline void SetBField3V(G4ThreeVector v) { BField3V = v; }
 	inline G4ThreeVector GetBField3V() const { return BField3V; }
 
-	inline void clearToscaFields(){
-		fFields.clear();
-	}
+	void clearToscaFields();
+	void SetTOSCAFields( std::vector<G4String> files , std::vector<G4double> scales , std::vector<G4double> beamlineOffsets);
 
 private:
 
+	bool bUseBFieldMaps;
 	std::vector<MolPolTOSCAField*> fFields;
 
 	void AddNewField(G4String& name,G4double scale,G4double zOffset);
-
-	bool bUseUniformEField;
-	bool bUseUniformBField;
-	bool bUseBFieldMaps;
 
 	G4double ErDC;
 	G4double ErInner;
