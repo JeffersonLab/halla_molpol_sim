3c44ed56dd59d4c8cf127aa78471b32eeafda9eb
dericking <dekin100@syr.edu>
2018-11-19 20:28:00 -0500

    Changes to MolPolDipole for Unified Field Types

--- include/MolPolDipole.hh
+++ include/MolPolDipole.hh
@@ -10,19 +10,26 @@ class MolPolDipole : public G4MagneticField
 {
 public: // with description
 
-  MolPolDipole(G4double          pBend);
-  MolPolDipole(G4double          pBend,
-               G4ThreeVector     pOrigin,
-               G4RotationMatrix* pMatrix);
+  MolPolDipole(G4double pBend, G4double pZeff);
+  MolPolDipole(G4double pBend, G4ThreeVector pOrigin, G4RotationMatrix* pMatrix, G4double pZeff);
   ~MolPolDipole();
 
-  void UpdateDipole(G4double, G4ThreeVector, G4RotationMatrix*);
-  void GetFieldValue(const G4double yTrack[],
-                     G4double B[]     ) const;
-private:
+  void updateDipole(G4double, G4ThreeVector, G4RotationMatrix*);
+  void GetFieldValue(const G4double yTrack[], G4double B[]) const;
 
+private:
   G4double          fBend;
-  G4ThreeVector     fOrigin;
-  G4RotationMatrix* fpMatrix;
+  G4ThreeVector     fOrigin = G4ThreeVector(0.,0.,423.4*cm);
+  G4RotationMatrix* fMatrix;
+  G4double          fZeff;
+
+public:
+  void setDipoleStrength(G4double pBend);
+  void setDipoleZeff(G4double pZeff);
+  void setRotationMatrix(G4RotationMatrix* pMatrix);
+  void setDipoleOrigin(G4ThreeVector pOrigin);
+  void updateDipole(G4double pBend, G4ThreeVector pOrigin, G4RotationMatrix* pMatrix, G4double pZeff);
+
+
 };
 #endif
