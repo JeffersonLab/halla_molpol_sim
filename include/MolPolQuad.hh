aa542f825f4b9fa6b2e066b7e7a8c020c52e0bd3
dericking <dekin100@syr.edu>
2018-11-19 20:32:52 -0500

    changes for unified field types

--- include/MolPolQuad.hh
+++ include/MolPolQuad.hh
@@ -7,24 +7,26 @@
 class MolPolQuad : public G4MagneticField
 {
 public: // with description
-
-  MolPolQuad(G4double          pGradient,
-             G4ThreeVector     pOrigin,
-             G4RotationMatrix* pMatrix,
-             G4double          pRadius);
-
+  MolPolQuad(G4double pGradient,G4ThreeVector pOrigin, G4RotationMatrix* pMatrix, G4double pRadius, G4double pZeff);
   ~MolPolQuad();
-
-  void GetFieldValue(const G4double yTrack[],
-                     G4double B[]     ) const;
+  void GetFieldValue(const G4double yTrack[], G4double B[] ) const;
   G4ThreeVector GetFringeField(G4ThreeVector) const;
-  void UpdateQuad(G4double, G4ThreeVector, G4RotationMatrix*, G4double);
-private:
+  void updateQuad(G4double pGradient,G4ThreeVector pOrigin, G4RotationMatrix* pMatrix, G4double pRadius, G4double pZeff);
+  void updateQuad(G4double pGradient);
 
+private:
   G4double          fGradient;
   G4ThreeVector     fOrigin;
-  G4RotationMatrix* fpMatrix;
+  G4RotationMatrix* fMatrix;
   G4double          fRadius;
+  G4double          fZeff;
+
+public:
+  void setQuadGradient(G4double);
+  void setQuadOrigin(G4ThreeVector);
+  void setQuadRotation(G4RotationMatrix*);
+  void setQuadBoreRadius(G4double);
+  void setQuadZeff(G4double);
 
 };
 #endif
