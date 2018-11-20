c8ff788d333f98a26f99e3e80511dc2dbd56c97c
dericking <dekin100@syr.edu>
2018-11-19 20:36:54 -0500

    EMField changes for unified field types. All fields now part of global object rather than local.

--- src/MolPolEMField.cc
+++ src/MolPolEMField.cc
@@ -14,7 +14,6 @@
 
 MolPolEMField::MolPolEMField()
 {
-
   EField3V.set(0,0,0);
   BField3V.set(0,0,0);
 }
@@ -28,38 +27,58 @@ MolPolEMField::~MolPolEMField()
 }
 
 
-////////////////////////////////////////////////////////////////////////////
-//input Point[4] (x,y,z,t)
-//
+//////////////////////////////////////////////////////////////  (╯°□°）╯︵ ┻━┻
+// Get Field Value
 inline void MolPolEMField::GetFieldValue(const G4double Point[4],G4double *Bfield) const
 {
-  //////////////////////////////////////////////////////////
-  //get BField
-  if(this->bUseUniformBField)
-    {
-      Bfield[0]=BField3V.x();
-      Bfield[1]=BField3V.y();
-      Bfield[2]=BField3V.z();
-    }
-  else
-    {
-      double pB[3],pPos[3]={Point[0]/cm,Point[1]/cm,Point[2]/cm};  //turn into cm
-      for(int i=0;i<3;i++) Bfield[i]=0.0;  //reset
+  // Member func is 'const' so everything used within must be declared within.
+  G4double Bsum[3]  = {0.,0.,0.};
+  G4double thisB[3] = {0.,0.,0.};
+  //////////////////////////////////////////////////////////////  (╯°□°）╯︵ ┻━┻
+  // B-Field
+  Bfield[0]=BField3V.x();
+  Bfield[1]=BField3V.y();
+  Bfield[2]=BField3V.z();
+  //G4cout << "CALCULATE THE FIELD VALUE......................................................................................." << G4endl;
+  // Get solenoid field
+  if(bBfieldInUse[0] == true){
+    cSolenoidField->GetFieldValue(Point,thisB);
+    //G4cout << "\nVolume Summary Solenoid @ P(" << Point[0]/cm << "," << Point[1]/cm << "," << Point[2]/cm << ") and B(" << thisB[0]/tesla << "," << thisB[1]/tesla << "," << thisB[2]/tesla << ")" << G4endl;
+    for (G4int i = 0; i < 3; i++) Bsum[i] += thisB[i];
+  }
+  if(bBfieldInUse[1] == true){
+    cQuad1Field->GetFieldValue(Point,thisB);
+    //G4cout << "Volume Summary Quad1 @ P(" << Point[0]/cm << "," << Point[1]/cm << "," << Point[2]/cm << ") and B(" << thisB[0]/tesla << "," << thisB[1]/tesla << "," << thisB[2]/tesla << ")" << G4endl;
+    for (G4int i = 0; i < 3; i++) Bsum[i] += thisB[i];
+  }
+  if(bBfieldInUse[2] == true){
+    cQuad2Field->GetFieldValue(Point,thisB);
+    //G4cout << "Volume Summary Quad2 @ P(" << Point[0]/cm << "," << Point[1]/cm << "," << Point[2]/cm << ") and B(" << thisB[0]/tesla << "," << thisB[1]/tesla << "," << thisB[2]/tesla << ")" << G4endl;
+    for (G4int i = 0; i < 3; i++) Bsum[i] += thisB[i];
+  }
+  if(bBfieldInUse[3] == true){
+    cQuad3Field->GetFieldValue(Point,thisB);
+    //G4cout << "Volume Summary Quad3 @ P(" << Point[0]/cm << "," << Point[1]/cm << "," << Point[2]/cm << ") and B(" << thisB[0]/tesla << "," << thisB[1]/tesla << "," << thisB[2]/tesla << ")" << G4endl;
+    for (G4int i = 0; i < 3; i++) Bsum[i] += thisB[i];
+  }
+  if(bBfieldInUse[4] == true){
+    cQuad4Field->GetFieldValue(Point,thisB);
+    //G4cout << "Volume Summary Quad4 @ P(" << Point[0]/cm << "," << Point[1]/cm << "," << Point[2]/cm << ") and B(" << thisB[0]/tesla << "," << thisB[1]/tesla << "," << thisB[2]/tesla << ")" << G4endl;
+    for (G4int i = 0; i < 3; i++) Bsum[i] += thisB[i];
+  }
+  if(bBfieldInUse[5] == true){
+    cDipoleField->GetFieldValue(Point,thisB);
+    //G4cout << "Volume Summary Dipole @ P(" << Point[0]/cm << "," << Point[1]/cm << "," << Point[2]/cm << ") and B(" << thisB[0]/tesla << "," << thisB[1]/tesla << "," << thisB[2]/tesla << ")" << G4endl;
+    for (G4int i = 0; i < 3; i++) Bsum[i] += thisB[i];
+  }
 
-    }
+  // Put Bsum into BField array which will
+  for (int i = 0; i < 3; i++) Bfield[i] = Bsum[i];
 
-  //////////////////////////////////////////////////////////
-  //get EFiled,
-
+  //////////////////////////////////////////////////////////////  (╯°□°）╯︵ ┻━┻
+  // E-Field -- if ever so desired
   double  *Efield=&Bfield[3];
-  if(this->bUseUniformEField)
-    {
-      Efield[0]=EField3V.x();
-      Efield[1]=EField3V.y();
-      Efield[2]=EField3V.z();
-    }
-  else
-    {
-      for(int i=0;i<3;i++) Efield[i]=0.;
-    }
+  Efield[0]=EField3V.x();
+  Efield[1]=EField3V.y();
+  Efield[2]=EField3V.z();
 }
