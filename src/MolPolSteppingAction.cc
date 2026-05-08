049711b12fb91ff26d427b39e3e2be0821b7b30b
Eric King <david.e.king@outlook.com>
2026-05-08 14:23:00 -0400

    removed two unused lines & general tidying on SteppingAction

--- src/MolPolSteppingAction.cc
+++ src/MolPolSteppingAction.cc
@@ -12,16 +12,12 @@ MolPolSteppingAction::MolPolSteppingAction()
 :fTrackMollersOnly(true),  //Default option, tracking all will substantially increase file sizes
  fStepActKryptEdge(false)
 {
-
 }
 
 void MolPolSteppingAction::UserSteppingAction(const G4Step *aStep) {
   G4Track * aTrack = aStep->GetTrack();
 
   G4String strPhysVolName = aTrack->GetVolume()->GetName();
-
-  std::size_t foundVB = strPhysVolName.find("virtualBoundaryPhys_det");
-  if( foundVB != G4String::npos ){aTrack->SetTrackStatus(fStopAndKill);}
 
   ///////////////////////////////////////////////////////////////  (╯°□°）╯︵ ┻━┻
   // Condition to track mollers only.
@@ -32,20 +28,18 @@ void MolPolSteppingAction::UserSteppingAction(const G4Step *aStep) {
   ///////////////////////////////////////////////////////////////  (╯°□°）╯︵ ┻━┻
   // Condition to make kryptonite-like edges without kryptonite.
   if(fStepActKryptEdge){
-
     G4bool isInGEM = (strPhysVolName.find("GEM.Tracking") != G4String::npos);
-  
-      if(aTrack->GetMaterial()->GetName() != "MP_Vacuum" &&
-         aTrack->GetMaterial()->GetName() != "MP_Air" &&
-         aTrack->GetMaterial()->GetName() != "MP_Scint" &&
-         aTrack->GetVolume()->GetName() != "Target"  &&
-         aTrack->GetVolume()->GetName() != "DipoleExitWindowR" &&
-         aTrack->GetVolume()->GetName() != "DipoleExitWindowL" &&
-        !isInGEM)
-      {
-        aTrack->SetTrackStatus(fStopAndKill);
-        return;
-      }
+    if(aTrack->GetMaterial()->GetName() != "MP_Vacuum" &&
+      aTrack->GetMaterial()->GetName() != "MP_Air" &&
+      aTrack->GetMaterial()->GetName() != "MP_Scint" &&
+      aTrack->GetVolume()->GetName() != "Target"  &&
+      aTrack->GetVolume()->GetName() != "DipoleExitWindowR" &&
+      aTrack->GetVolume()->GetName() != "DipoleExitWindowL" &&
+     !isInGEM)
+    {
+      aTrack->SetTrackStatus(fStopAndKill);
+      return;
     }
+  }
 
 }
